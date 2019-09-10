1613a6107d44bd84b0efaff76abb3b58ab6f08f7
Adam <adam.ponting@gmail.com>
2019-09-10 14:17:24 +0100

    navbar done for mobile

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="HomeBanner">
+    <div class="image" alt>
+      <base-icon
+        name="arrow-down-circle"
+        class="icon"
+        v-scroll-to="{
+          el: '#element',
+          duration: 1000
+        }"
+      ></base-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeBanner'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.image {
+  width: 100%;
+  height: 100vh;
+  background-image: url('https://firebasestorage.googleapis.com/v0/b/lanista-run.appspot.com/o/running-banner-image.jpg?alt=media&token=f5246ce0-7f45-42f2-a0cb-66e99f557938"');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 70% center;
+  background-attachment: scroll;
+}
+
+.icon {
+  color: white;
+  position: absolute;
+  bottom: 10%;
+  left: 50%;
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/views/TrainingPlans.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>training plans</div>
+</template>
+
+<script>
+export default {
+  name: 'TrainingPlans'
+}
+</script>
+
+<style lang="scss" scoped></style>
