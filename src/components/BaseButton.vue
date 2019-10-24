0395c159e86f6b4e17d97a33b462a590920a28a1
Adam Ponting <adam.ponting@gmail.com>
2019-10-24 20:42:19 +0100

    created base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="button" :type="type">
+    <slot name="name"></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+    name: {
+      type: String,
+      default: 'defaultButton'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/css/app.scss';
+
+.button {
+  background-color: $button-info;
+  color: $text-light;
+
+  display: inline-flex;
+
+  padding: 0.5em;
+
+  align-items: center;
+  text-transform: uppercase;
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
