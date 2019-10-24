0203d389086aa2e8cf4bf24fd944d4b751dd8b1b
Adam Ponting <adam.ponting@gmail.com>
2019-10-24 20:41:07 +0100

    made into array

--- src/assets/data/GettingStarted.js
+++ src/assets/data/GettingStarted.js
@@ -1,113 +1,56 @@
-export const plan = {
-  week1: [
-    { weekNumber: 'One' },
-    {
-      plan: [
-        {
-          id: 1,
-          activity:
-            'Run one min, walk 90 seconds. Repeat eight times. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week2: [
-    { weekNumber: 'Two' },
-    {
-      plan: [
-        {
-          id: 2,
-          activity:
-            'Run two mins, walk one min. Repeat seven times. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week3: [
-    { weekNumber: 'Three' },
-    {
-      plan: [
-        {
-          id: 3,
-          activity:
-            'Run three mins, walk one min. Repeat six times. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week4: [
-    { weekNumber: 'Four' },
-    {
-      plan: [
-        {
-          id: 4,
-          activity:
-            'Run five mins, walk two mins. Repeat four times. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week5: [
-    { weekNumber: 'Five' },
-    {
-      plan: [
-        {
-          id: 5,
-          activity:
-            'Run eight mins, walk two mins. Repeat three times. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week6: [
-    { weekNumber: 'six' },
-    {
-      plan: [
-        {
-          id: 6,
-          activity:
-            'Run 12 mins, walk one min. Repeat three times. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week7: [
-    { weekNumber: 'Seven' },
-    {
-      plan: [
-        {
-          id: 7,
-          activity:
-            'Run 15 mins, walk one min, run 15 mins. Do three times a week.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ],
-  week8: [
-    { weekNumber: 'Eight' },
-    {
-      plan: [
-        {
-          id: 8,
-          activity: 'Run 30 mins continuously.',
-          comment: 'none',
-          isComplete: false
-        }
-      ]
-    }
-  ]
-}
+export const gettingStarted = [
+  {
+    id: 1,
+    activity:
+      'Run one min, walk 90 seconds. Repeat eight times. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 2,
+    activity:
+      'Run two mins, walk one min. Repeat seven times. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 3,
+    activity:
+      'Run three mins, walk one min. Repeat six times. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 4,
+    activity:
+      'Run five mins, walk two mins. Repeat four times. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 5,
+    activity:
+      'Run eight mins, walk two mins. Repeat three times. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 6,
+    activity:
+      'Run 12 mins, walk one min. Repeat three times. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 7,
+    activity: 'Run 15 mins, walk one min, run 15 mins. Do three times a week.',
+    comment: 'none',
+    isComplete: false
+  },
+  {
+    id: 8,
+    activity: 'Run 30 mins continuously.',
+    comment: 'none',
+    isComplete: false
+  }
+]
